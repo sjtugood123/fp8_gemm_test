e8ef3c49bd01f8b83d5147a71b5fe0fe8f3cf157
zhaoxvte <zhaoxvte@sjtu.edu.cn>
2026-03-07 17:31:57 +0800

    change file structure

---
+++ paper_test/compile_run.sh
@@ -0,0 +1,71 @@
+# nvcc -diag-suppress 550 -O3 -std=c++17 -arch=sm_120a \
+# -I/home/xtzhao/cutlass/include \
+# -I/home/xtzhao/cutlass/tools/util/include/ \
+# -I/home/xtzhao/cutlass/examples/common/ \
+# -I/home/xtzhao/cutlass/include/cute \
+# -o bin/fp8_acc_bitwidth fp8_acc_bitwidth.cu -lcudart
+
+# nvcc -diag-suppress 550 -O3 -std=c++17 -arch=sm_120a \
+# -I/home/xtzhao/cutlass/include \
+# -I/home/xtzhao/cutlass/tools/util/include/ \
+# -I/home/xtzhao/cutlass/examples/common/ \
+# -I/home/xtzhao/cutlass/include/cute \
+# -o bin/fp4_acc_bitwidth fp4_acc_bitwidth.cu -lcudart
+
+# nvcc -diag-suppress 550 -O3 -std=c++17 -arch=sm_120a \
+# -I/home/xtzhao/cutlass/include \
+# -I/home/xtzhao/cutlass/tools/util/include/ \
+# -I/home/xtzhao/cutlass/examples/common/ \
+# -I/home/xtzhao/cutlass/include/cute \
+# -o bin/test_mxfp4 test_mxfp4.cu -lcudart
+
+
+# nvcc -diag-suppress 550 -O3 -std=c++17 -arch=sm_120a \
+# -I/home/xtzhao/cutlass/include \
+# -I/home/xtzhao/cutlass/tools/util/include/ \
+# -I/home/xtzhao/cutlass/examples/common/ \
+# -I/home/xtzhao/cutlass/include/cute \
+# -o bin/fp4_block_scalevec1 fp4_block_scalevec1.cu -lcudart
+
+# nvcc -diag-suppress 550 -diag-suppress 177 -O3 -std=c++17 -arch=sm_120a \
+# -I/home/xtzhao/cutlass/include \
+# -I/home/xtzhao/cutlass/tools/util/include/ \
+# -I/home/xtzhao/cutlass/examples/common/ \
+# -I/home/xtzhao/cutlass/include/cute \
+# -o bin/fp4_block_scalevec2 fp4_block_scalevec2.cu -lcudart
+
+# nvcc -diag-suppress 550 -O3 -std=c++17 -arch=sm_120a \
+# -I/home/xtzhao/cutlass/include \
+# -I/home/xtzhao/cutlass/tools/util/include/ \
+# -I/home/xtzhao/cutlass/examples/common/ \
+# -I/home/xtzhao/cutlass/include/cute \
+# -o bin/largest_k_instr largest_k_instr.cu -lcudart
+
+# nvcc -diag-suppress 550 -O3 -std=c++17 -arch=sm_120a \
+# -I/home/xtzhao/cutlass/include \
+# -I/home/xtzhao/cutlass/tools/util/include/ \
+# -I/home/xtzhao/cutlass/examples/common/ \
+# -I/home/xtzhao/cutlass/include/cute \
+# -o bin/fp16_lossless fp16_lossless.cu -lcudart
+
+nvcc -diag-suppress 550 -O3 -std=c++17 -arch=sm_120a \
+-I/home/xtzhao/cutlass/include \
+-I/home/xtzhao/cutlass/tools/util/include/ \
+-I/home/xtzhao/cutlass/examples/common/ \
+-I/home/xtzhao/cutlass/include/cute \
+-o bin/W_probe W_probe.cu -lcudart
+
+# ./bin/fp8_acc_bitwidth
+# ./bin/fp4_acc_bitwidth
+# ./bin/test_mxfp4
+# ./bin/fp4_block_scalevec1
+# ./bin/fp4_block_scalevec2
+# ./bin/largest_k_instr
+# ./bin/fp16_lossless
+./bin/W_probe
+
+# cuobjdump -sass ./bin/fp8_acc_bitwidth > ./sass/fp8.sass
+# cuobjdump -sass ./bin/fp4_acc_bitwidth > ./sass/fp4.sass
+# cuobjdump -sass ./bin/test_mxfp4 > ./sass/test_mxfp4.sass
+# cuobjdump -sass ./bin/fp4_block_scalevec1 > ./sass/fp4_block_scalevec1.sass
+# cuobjdump -sass ./bin/fp4_block_scalevec2 > ./sass/fp4_block_scalevec2.sass
